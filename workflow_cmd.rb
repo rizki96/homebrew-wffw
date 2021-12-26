40e29697096a6fb4e079eb2182b0bcda31d76250
Iskandar Rizki <iskandar.rizki@gmail.com>
2021-12-27 04:15:58 +0700

    fix workflow_cmd script

--- workflow_cmd.rb
+++ workflow_cmd.rb
@@ -1,5 +1,5 @@
 class WorkflowCmd < Formula
-  desc "Running the JSON workflow DSL via command-line""
+  desc "Running the JSON workflow DSL via command-line"
   homepage "https://github.com/rizki96/workflow_cmd"
   url "https://github.com/rizki96/workflow_cmd/releases/download/v0.1.0/workflow_cmd-0.1.0.tar.gz"
   sha256 "9c9150a153b021bb84cdaec1388de01800d2abd2eb2642a38798c5d0c3e77b48"
