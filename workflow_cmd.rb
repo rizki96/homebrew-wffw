efcff169fdb498752cf0a478b9f615b69aa70514
Iskandar Rizki <iskandar.rizki@gmail.com>
2021-12-27 04:13:31 +0700

    add workflow_cmd into homebrew tap

---
+++ workflow_cmd.rb
@@ -0,0 +1,17 @@
+class WorkflowCmd < Formula
+  desc "Running the JSON workflow DSL via command-line""
+  homepage "https://github.com/rizki96/workflow_cmd"
+  url "https://github.com/rizki96/workflow_cmd/releases/download/v0.1.0/workflow_cmd-0.1.0.tar.gz"
+  sha256 "9c9150a153b021bb84cdaec1388de01800d2abd2eb2642a38798c5d0c3e77b48"
+
+  bottle :unneeded
+
+  def install
+    bin.install "workflow_cmd"
+  end
+
+  test do
+    system "#{bin}/workflow_cmd"
+  end
+end
+
