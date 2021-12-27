596af0d4c7a1d6d09f071443ce878116f74ab975
Iskandar Rizki <iskandar.rizki@gmail.com>
2021-12-27 08:57:16 +0700

    remove bottle :unneeded

--- workflow_cmd.rb
+++ workflow_cmd.rb
@@ -4,7 +4,7 @@ class WorkflowCmd < Formula
   url "https://github.com/rizki96/workflow_cmd/releases/download/v0.1.0/workflow_cmd-0.1.0.tar.gz"
   sha256 "9c9150a153b021bb84cdaec1388de01800d2abd2eb2642a38798c5d0c3e77b48"
 
-  bottle :unneeded
+  #bottle :unneeded
 
   def install
     bin.install "workflow_cmd"
