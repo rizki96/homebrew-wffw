c4df59d5625786ceaf5ca242ce800336fe0fb0a5
iskandar.rizki <iskandar.rizki@rumahsiapkerja.com>
2022-06-13 10:28:40 +0700

    update sha sums

--- workflow_cmd.rb
+++ workflow_cmd.rb
@@ -1,11 +1,10 @@
 class WorkflowCmd < Formula
-  version "0.4.5"
-  sha256 "d147af7d2177bc89b2eda48e86b5e70fbe3b9211ea69dab38e45c1ff37496234"
+  version "0.4.6"
 
   desc "Running the JSON workflow DSL via command-line"
   homepage "https://github.com/rizki96/workflow_cmd"
   url "https://github.com/rizki96/workflow_cmd/releases/latest/download/workflow_cmd.tar.gz"
-  sha256 "a57515722a4b7102d025abf65a0a3bbb21af0d0fdc93c27b3494629ae4183c26"
+  sha256 "ad72360b89ef1ff95ac89bf6a90859f62cad5f91d1c3205ec84fa333cf566438"
 
   #bottle :unneeded
 
