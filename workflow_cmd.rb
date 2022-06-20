47711796003ed4c8724ee250fee964c9810cda6f
iskandar.rizki <iskandar.rizki@rumahsiapkerja.com>
2022-06-21 03:34:02 +0700

    upgrade version to 0.4.8

--- workflow_cmd.rb
+++ workflow_cmd.rb
@@ -1,10 +1,10 @@
 class WorkflowCmd < Formula
-  version "0.4.6"
+  version "0.4.8"
 
   desc "Running the JSON workflow DSL via command-line"
   homepage "https://github.com/rizki96/workflow_cmd"
   url "https://github.com/rizki96/workflow_cmd/releases/latest/download/workflow_cmd.tar.gz"
-  sha256 "ad72360b89ef1ff95ac89bf6a90859f62cad5f91d1c3205ec84fa333cf566438"
+  sha256 "4662c55d04c11c61ecdbe759c1798e2c12a69b726c375171fab302c7d3762141"
 
   #bottle :unneeded
 
